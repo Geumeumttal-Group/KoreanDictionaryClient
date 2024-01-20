501d42bd4152270854090e29c332f63cfd69f77d
Weymiens N <156708849+weymini@users.noreply.github.com>
2024-01-20 21:45:09 +0100

    Add KoreanDictionaryClient Add initial version of the abstract KoreanDictionaryClass. Currently only has a read-only field: _apiKey.

---
+++ GeumeumttalGroup.KoreanDictionaryClient.Core/KoreanDictionaryClient.cs
@@ -0,0 +1,13 @@
+﻿namespace GeumeumttalGroup.KoreanDictionaryClient.Core;
+
+/// <summary>
+/// Represents an abstract base class for Korean dictionary clients.
+/// </summary>
+/// <param name="apiKey">The API key that allows your client to access the Korean dictionary API.</param>
+public abstract class KoreanDictionaryClient(string apiKey)
+{
+    /// <summary>
+    /// The API key that allows your client to access the Korean dictionary API.
+    /// </summary>
+    private readonly string _apiKey = apiKey;
+}
