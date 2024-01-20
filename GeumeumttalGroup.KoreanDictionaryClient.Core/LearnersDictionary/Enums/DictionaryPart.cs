d064033f3e8aea18ae45af2defe1434cb5532d6c
Weymiens N <156708849+weymini@users.noreply.github.com>
2024-01-20 21:47:07 +0100

    Add DictionaryPart Create enum DictionaryPart. This is an enumeration of values used for API parameter "sort". Has extension method to transform the enum to the string the API expects.

---
+++ GeumeumttalGroup.KoreanDictionaryClient.Core/LearnersDictionary/Enums/DictionaryPart.cs
@@ -0,0 +1,41 @@
+﻿namespace GeumeumttalGroup.KoreanDictionaryClient.Core.LearnersDictionary;
+
+/// <summary>
+/// An enum of avaible parts of the dictionary in which a search can be requested.
+/// </summary>
+public enum DictionaryPart
+{
+    /// <summary>
+    /// [Default] A search based on the word itself.
+    /// </summary>
+    Word,
+    /// <summary>
+    /// A search based on idiomatic expressions and/or proverbs.
+    /// </summary>
+    IdiomProverb,
+    /// <summary>
+    /// A search based on definitions.
+    /// </summary>
+    Definition,
+    /// <summary>
+    /// A search based on exam utilization.
+    /// </summary>
+    Exam
+}
+
+public static class DictionaryPartExtension
+{
+    /// <summary>
+    /// Converts the enum to its corresponding string value utilized by the Korean Learners Dictionary API.
+    /// </summary>
+    public static string ToApiValue(this DictionaryPart dictionaryPart)
+    {
+        return dictionaryPart switch
+        {
+            DictionaryPart.IdiomProverb => "ip",
+            DictionaryPart.Definition => "dfn",
+            DictionaryPart.Exam => "exam",
+            _ => "word"
+        };
+    }
+}
