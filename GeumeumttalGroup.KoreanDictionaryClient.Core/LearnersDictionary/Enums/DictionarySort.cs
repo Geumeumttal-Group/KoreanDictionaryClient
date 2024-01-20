eb32e8ddb4c5ae72873d4f4790a60ba68e231c80
Weymiens N <156708849+weymini@users.noreply.github.com>
2024-01-20 21:48:20 +0100

    Add DictionarySort Create enum DictionarySort. This is an enumeration of values used for API parameter "dict". Has extension method to transform the enum to the string the API expects.

---
+++ GeumeumttalGroup.KoreanDictionaryClient.Core/LearnersDictionary/Enums/DictionarySort.cs
@@ -0,0 +1,31 @@
+﻿namespace GeumeumttalGroup.KoreanDictionaryClient.Core.LearnersDictionary;
+
+/// <summary>
+/// An enum of avaible sorting methods for sorting the search results.
+/// </summary>
+public enum DictionarySort
+{
+    /// <summary>
+    /// [Default] Sort search results alphabetically.
+    /// </summary>
+    Dictionary,
+    /// <summary>
+    /// Sort search results by populartity of the specific results.
+    /// </summary>
+    Popularity
+}
+
+public static class SortExtention
+{
+    /// <summary>
+    /// Converts the enum to its corresponding string value utilized by the Korean Learners Dictionary API.
+    /// </summary>
+    public static string ToApiValue(this DictionarySort sort)
+    {
+        if (sort == DictionarySort.Dictionary) 
+        {
+            return "dict";
+        }
+        return "popular";
+    }
+}
